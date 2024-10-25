0f0a86af6f8dbe10e241353737a264481e720c76
keyp-dev <kaipan.1314.666@gmail.com>
2024-10-25 19:22:18 +0800

    chore: update comment

--- src/index.ts
+++ src/index.ts
@@ -11,62 +11,59 @@ const createGiftBox = async (
   },
   artwork_nft: ArtworkNFT
 ) => {
-  // Set up API request options
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      // Using staging environment API key, if you want to use prod environment, you need to use prod API key
-      "x-api-key": "HVdFMEJcViKrIhvdfxfe3",
-    },
-    body: JSON.stringify({
-      transfer_method: "NFTVault",
-      has_sponsor: true,
-      artworks: [
-        {
-          // NFT-related information
-          nft_contract: artwork_nft.contract,
-          token_id: artwork_nft.token_id,
-          source: artwork_nft.mediaUrl,
-          source_metadata: artwork_nft.mediaMetadata,
-          minted_on: new Date().toISOString(),
 
-          // Artist and artwork information
-          artist_name: "Annibale Siconolfi",
-          artist_description: "",
-          artist_avatar:
-            "https://ipfs.pixura.io/ipfs/QmaQhxcVm8GeCK9355qBxpEERXNe5avaLHbZwGnY3vhHJq/hidden%20city.jpg",
-          artwork_name: "Demo Artwork name",
-          artwork_description: "Demo Artwork description",
-
-          type: artwork_nft.type,
-          // Only support 1, indicating one 1155/721 NFT
-          amount: 1,
-        },
-      ],
-      // Gift box configuration
-      gift_box: {
-        // Text display for corresponding positions on the page
-        note_subject: "To test",
-        message: "Thank you for joining Gifted.art",
-        message_bless: " ",
-        signature: "—Gifted.art Team",
-
-        // UI-related configurations for the page
-        // Wrapping animation, if you need more, please go to https://app.gifted.art/, follow the process to send a gift, and you can see more wrapping styles at the last step
-        wrapping: "Silver Elegance",
-      },
-      recipient,
-    }),
-  };
-
-  // Send API request to create gift box
   const res = await fetch(
+    // this is staging environment api, prod environment api is https://rest-api.gifted.art/api/v1/contracts:createGiftBox
     "https://rest-api-staging.gifted.art/api/v1/contracts:createGiftBox",
-    options
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // Using staging environment API key, if you want to use prod environment, you need to use prod API key
+        "x-api-key": "HVdFMEJcViKrIhvdfxfe3",
+      },
+      body: JSON.stringify({
+        transfer_method: "NFTVault",
+        has_sponsor: true,
+        artworks: [
+          {
+            // NFT-related information
+            nft_contract: artwork_nft.contract,
+            token_id: artwork_nft.token_id,
+            source: artwork_nft.mediaUrl,
+            source_metadata: artwork_nft.mediaMetadata,
+            minted_on: new Date().toISOString(),
+
+            // Artist and artwork information
+            artist_name: "Annibale Siconolfi",
+            artist_description: "",
+            artist_avatar:
+              "https://ipfs.pixura.io/ipfs/QmaQhxcVm8GeCK9355qBxpEERXNe5avaLHbZwGnY3vhHJq/hidden%20city.jpg",
+            artwork_name: "Demo Artwork name",
+            artwork_description: "Demo Artwork description",
+
+            type: artwork_nft.type,
+            // Only support 1, indicating one 1155/721 NFT
+            amount: 1,
+          },
+        ],
+        // Gift box configuration
+        gift_box: {
+          // Text display for corresponding positions on the page
+          note_subject: "To test",
+          message: "Thank you for joining Gifted.art",
+          message_bless: " ",
+          signature: "—Gifted.art Team",
+
+          // UI-related configurations for the page
+          // Wrapping animation, if you need more, please go to https://app.gifted.art/, follow the process to send a gift, and you can see more wrapping styles at the last step
+          wrapping: "Silver Elegance",
+        },
+        recipient,
+      }),
+    }
   );
 
-  // Handle API response
   if (!res.ok) {
     const data = await res.json();
     console.error(res.statusText);
@@ -78,32 +75,27 @@ const createGiftBox = async (
   console.log(await res.text());
 };
 
-// Main function
 const main = async () => {
-  // Get NFTs for the specified owner
   const nfts = await getNFTsByOwner({
     // Only supports base / base-sepolia, using base-sepolia for staging environment, use base for prod environment
     chain: "base-sepolia",
-    // This is the NFTVault address
+    // This is the NFTVault address, this is the test environment NFTVault address, we will provide the prod NFTVault address later
     owner: "0x642A0D79bc1842290ee0B893811b93c52c3A4A0F",
     // base-sepolia 1155 contract address (if you want to send 721 NFT, you need to use the 721 contract address)
     contract: "0x2Faa4ff5Ee8D3D47915ABe87Ae44f550448A4CB0",
   });
 
-  // Check if any NFTs were found
   if (nfts.length === 0) {
     console.error("No NFTs found");
     return;
   }
 
-  // Set up recipient information
   const recipient = {
     email: "test1@keyp.dev",
     first_name: "keyp",
     last_name: "test",
   };
 
-  // Create a gift box using the first NFT
   await createGiftBox(recipient, nfts[0]);
 };
 
